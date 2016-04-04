67fd9b50c6f464245803ea23661f372517ef09f5
lewisek <lewi666@wp.pl>
2016-04-04 23:02:13 +0200

    Added files via upload

---
+++ dane2.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+require('simple_html_dom.php');
+
+$html = file_get_html("http://chojnice24.pl/");
+
+
+foreach($html->find('div[id=r_lotto]') as $element) 
+       echo $element;
+	   
+foreach($html->find('div[id=r_waluty]') as $element) 
+       echo $element;
+	   	   
+foreach($html->find('div[id=r_sonda]') as $element) 
+       echo $element;
+	   //echo $html;
+
+
+?>
+
